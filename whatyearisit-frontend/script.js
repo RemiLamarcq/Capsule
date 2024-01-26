528a8b5fa294c0bc7b53ed72ee8d2e927ae111e8
Rémi <remi.lamarcq@yahoo.fr>
2024-01-26 15:05:07 +0100

    test

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -5,6 +5,6 @@ fetch('https://capsule-flame.vercel.app/year')
 .then(response => response.json())
 .then(data => {
     console.log(data.year)
-    document.querySelector('#year').innerHTML = `<p>${data.year}<p>`
+    document.querySelector('#year').textContent = data.year
 })
 
