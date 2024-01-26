5d539e318025b79bb133c3faac2ac796e58a9e7c
Rémi <remi.lamarcq@yahoo.fr>
2024-01-26 15:04:47 +0100

    test

--- whatyearisit-backend/routes/year.js
+++ whatyearisit-backend/routes/year.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    const Nyear = new Date
-    year = Nyear.getFullYear();
-    console.log(year.yea)
+    const Nyear = new Date()
+    let year = Nyear.getFullYear().toString();
+
     res.json({ year })
 });
 
